85277fa6e08896b548de97ecc6bf191200bfc8ab
mhlee80 <myeonghyeon.lee@gmail.com>
2019-03-17 17:32:40 +0900

    [UPDATE] MainActivity
    주사위 이미지 추가
    주사위 이미지 뷰 추가
    랜덤 수에 맞는 주사위 이미지 표시

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -27,10 +28,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultText: TextView = result_text
-
         val randomInt = Random().nextInt(6) + 1
 
-        resultText.text = randomInt.toString()
+//        val resultText: TextView = result_text
+//        resultText.text = randomInt.toString()
+
+
+//        val diceImage: ImageView = findViewById<ImageView>(R.id.dice_image)
+        val diceImage: ImageView = dice_image
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
     }
 }
