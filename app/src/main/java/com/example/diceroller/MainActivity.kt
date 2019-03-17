19305817c87867d08b584d45f22cdd521fc4f30c
mhlee80 <myeonghyeon.lee@gmail.com>
2019-03-17 17:17:12 +0900

    [UPDATE] MainActivity - Roll dice
    Text view에 id 추가
    버튼 클릭시 랜덤 숫자를 text view에 표시

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.diceroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,7 +21,16 @@ class MainActivity : AppCompatActivity() {
         rollButton.text = "Let's Roll"
 
         rollButton.setOnClickListener {
-            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = result_text
+
+        val randomInt = Random().nextInt(6) + 1
+
+        resultText.text = randomInt.toString()
     }
 }
